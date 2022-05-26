420a7b41fa51c02aad0e84ac8cd0a0af9c7b023b
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-27 01:17:36 +0200

    Added buttons.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
